3553ebf8d2e9f589023b649a0bd70fa41dee7d37
MD Towhidul Islam <tuhintowhidul9@gmail.com>
2025-08-17 03:53:43 +0000

    Enhance Kubernetes deployment and update local configs
    *   Add support for populating Kubernetes ConfigMaps from `.env` in `deploy-kubernates.sh`.
    *   Update `docker-compose.yml` to use `go-nms` image for the worker and add `app` service dependency.
    *   Switch to `app-config-prod.json` for SSM configuration upload.
    *   Add `TELEGRAM_ENABLED` environment variable to worker Kubernetes deployment.
    *   Improve `port_forward.sh` by explicitly mapping ports.
    *   Update `.gitignore` to include new config files.

--- deploy-kubernates.sh
+++ deploy-kubernates.sh
@@ -3,6 +3,7 @@
 PROJECT_BASE_PATH=$(pwd)
 
 SECRETS_SED_COMMANDS=""
+CONFIGMAP_SED_COMMANDS=""
 
 while IFS='=' read -r key value; do
   if [[ -z "$key" || -z "$value" ]]; then
@@ -14,6 +15,11 @@ while IFS='=' read -r key value; do
     SECRETS_SED_COMMANDS+=" -e \"s|  $key: \".*\"|  $key: \\\"$ENCODED_VALUE\\\"|g\""
     SECRETS_SED_COMMANDS+=" -e \"s|  $key: .*|  $key: \\\"$ENCODED_VALUE\\\"|g\""
   fi
+
+  if grep -q "  $key:" k8/config-maps.yaml; then
+    CONFIGMAP_SED_COMMANDS+=" -e \"s|  $key: \".*\"|  $key: \\\"$value\\\"|g\""
+    CONFIGMAP_SED_COMMANDS+=" -e \"s|  $key: .*|  $key: \\\"$value\\\"|g\""
+  fi
 done < .env
 
 if [[ -n "$SECRETS_SED_COMMANDS" ]]; then
@@ -24,12 +30,20 @@ else
   kubectl apply -f k8/secrets.yaml
 fi
 
+if [[ -n "$CONFIGMAP_SED_COMMANDS" ]]; then
+  echo "Updating k8/config-maps.yaml with values from .env..."
+  eval "cat k8/config-maps.yaml | sed $CONFIGMAP_SED_COMMANDS" | kubectl apply -f -
+else
+  echo "No matching config map values found in .env to update k8/config-maps.yaml."
+  kubectl apply -f k8/config-maps.yaml
+fi
+
 kubectl create configmap prometheus-config --from-file=prometheus.yml=prometheus/prometheus.yml
 kubectl create configmap keycloak-import --from-file=gocloak-realm.json=keycloak/import/gocloak-realm.json
 
 for file in k8/*.yaml; do
-  if [ "$file" == "k8/secrets.yaml" ]; then
-    echo "Skipping k8/secrets.yaml as it's already processed."
+  if [ "$file" == "k8/secrets.yaml" ] || [ "$file" == "k8/config-maps.yaml" ]; then
+    echo "Skipping $file as it's already processed."
     continue
   fi
 
--- port_forward.sh
+++ port_forward.sh
@@ -7,8 +7,8 @@ SSH_USER="towhidul"
 LOCAL_PORT1="8080"
 LOCAL_PORT2="8081"
 
-kubectl port-forward service/app :8080 &
-kubectl port-forward service/keycloak :8081 &
+kubectl port-forward service/app 8080:8080 &
+kubectl port-forward service/keycloak 8081:8081 &
 
 ssh -p $SSH_PORT \
     -R $LOCAL_PORT1:localhost:$LOCAL_PORT1 \
